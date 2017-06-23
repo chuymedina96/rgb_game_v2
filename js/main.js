b7fe3a6d681f24899383a09310fa457831d7f3b2
Jesus Medina <chuymedina96@gmail.com>
2017-06-23 04:21:37 +0000

    added random color generator

--- js/main.js
+++ js/main.js
@@ -1,11 +1,4 @@
-var colors = [
-    "rgb(255, 0, 0)",
-    "rgb(255, 255, 0)",
-    "rgb(0, 255, 0)",
-    "rgb(0, 255, 255)",
-    "rgb(0, 0, 255)",
-    "rgb(255, 0, 255)",
-];
+var colors = generateRandomColors(6);
 
 var squares = document.querySelectorAll(".square");
 var pickedColor = pickNumber();
@@ -42,19 +35,15 @@ function pickNumber() {
 }
 function generateRandomColors(num){
     var arr = [];
-    for( var i = 0; i < num.length; i++) {
-        
+    for( var i = 0; i < num; i++) {
+        arr.push(randomColor());
     };
     return arr;
     
 }
 function randomColor() {
-    Math.floor(Math.random() * 266);
-    Math.floor(Math.random() * 266);
-    Math.floor(Math.random() * 266);
-    Math.floor(Math.random() * 266);
-    Math.floor(Math.random() * 266);
-    Math.floor(Math.random() * 266);
-    
-    
+    var r = Math.floor(Math.random() * 256);
+    var g = Math.floor(Math.random() * 256);
+    var b = Math.floor(Math.random() * 256);
+    return "rgb(" + r + ", " + g + ", " + b + ")";
 }
